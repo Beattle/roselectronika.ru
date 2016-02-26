43c599e44bc370e6c697551adcb4b2e481a1d1ef
Hipno <hipno@ruelectro.ru>
2016-02-26 23:48:39 +0300

    remove

--- bitrix/stack_cache/MYSQL/currency_rate/61/617ce81921b8be705ca13e5be3992c16.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001456088465';
-$dateexpire = '001456099265';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:4:{s:22:"C_R_2016-02-23_BYR_RUB";a:4:{s:11:"curFromRate";d:36.719999999999998863131622783839702606201171875;s:14:"curFromRateCnt";i:10000;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2016-02-23_EUR_RUB";a:4:{s:11:"curFromRate";d:43.7999999999999971578290569595992565155029296875;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2016-02-23_UAH_RUB";a:4:{s:11:"curFromRate";d:39.409999999999996589394868351519107818603515625;s:14:"curFromRateCnt";i:10;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2016-02-23_USD_RUB";a:4:{s:11:"curFromRate";d:32.2999999999999971578290569595992565155029296875;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}}}';
-return true;
-?>
